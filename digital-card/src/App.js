87fb2441c172cca40a6242cd16e43d4f90e10349
alain17-web <web2020.alain@gmail.com>
2022-03-29 11:24:02 +0200

    photo

---
+++ digital-card/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Photo from './components/Photo';
+
+function App() {
+  return (
+    <div className="container">
+      <Photo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ digital-card/src/components/Photo.js
@@ -0,0 +1,11 @@
+import photo from "../image/photo.JPG"
+
+function Photo(){
+    return (
+         
+            <img src={photo} alt="Alain Roos" className="photo"/>
+        
+    );
+}
+
+export default Photo;
